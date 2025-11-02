ef2b6d471483e34aaba37663a5b78bf4480c52ba
Sruu <patilketankumar87@gmail.com>
2025-11-02 22:13:44 +0530

    chore: add .gitattributes and renormalize line endings

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="grid grid-cols-5 h-screen text-center">
+      <div className="col-span-1 bg-zinc-800">
+      </div>
+
+      <div className="col-span-4 p-10 ">
+        <div className='container h-170 w-1/2' >
+        </div>
+          <div className='bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl
+           border border-zinc-100 flex h-16'>
+            <input type="text" className='w-full h-full p-3 outline-none' placeholder='Ask me Anything' />
+            <button>Ask</button>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
